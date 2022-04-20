df50cf1ed88325d9909da9af458eb5f12ee9c8db
Javoh29 <zhavokhirbek.ergashov@ent-en.com>
2022-04-20 11:14:59 +0500

    update

--- lib/src/widgets/error_info_page.dart
+++ lib/src/widgets/error_info_page.dart
@@ -11,56 +11,58 @@ class ErrorInfoPage extends ViewModelBuilderWidget<ErrorsViewModel> {
   @override
   Widget builder(
       BuildContext context, ErrorsViewModel viewModel, Widget? child) {
-    return Scaffold(
-      backgroundColor: Colors.grey[800],
-      appBar: AppBar(
-        backgroundColor: Colors.grey[900],
-        title: const Text(
-          'JBaza Exception',
-          style: TextStyle(
-              color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600),
-        ),
-        leading: IconButton(
-            onPressed: () => Navigator.pop(context),
-            icon: const Icon(
-              Icons.arrow_back_ios,
-              size: 22,
-              color: Colors.white,
-            )),
-        actions: [
-          IconButton(
-              onPressed: () => viewModel.shareError([_vmException]),
+    return Material(
+      child: Scaffold(
+        backgroundColor: Colors.grey[800],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[900],
+          title: const Text(
+            'JBaza Exception',
+            style: TextStyle(
+                color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600),
+          ),
+          leading: IconButton(
+              onPressed: () => Navigator.pop(context),
               icon: const Icon(
-                Icons.share,
+                Icons.arrow_back_ios,
                 size: 22,
                 color: Colors.white,
-              ))
-        ],
-      ),
-      body: ListView(
-        physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-        children: [
-          _item('TAG:', _vmException.tag),
-          const SizedBox(height: 15),
-          _item('Create time:', _vmException.time),
-          const SizedBox(height: 15),
-          _item('Call func name:', _vmException.callFuncName),
-          const SizedBox(height: 15),
-          _item('Line:', _vmException.line),
-          const SizedBox(height: 15),
-          _item('Base request', _vmException.baseRequest),
-          const SizedBox(height: 15),
-          _item('Response status code:', _vmException.responseStatusCode),
-          const SizedBox(height: 15),
-          _item('Response phrase:', _vmException.responsePhrase),
-          const SizedBox(height: 15),
-          _item('Response body:', _vmException.responseBody),
-          const SizedBox(height: 15),
-          _item('Token is valid:', _vmException.tokenIsValid),
-          const SizedBox(height: 15),
-          _item('Message:', _vmException.message)
-        ],
+              )),
+          actions: [
+            IconButton(
+                onPressed: () => viewModel.shareError([_vmException]),
+                icon: const Icon(
+                  Icons.share,
+                  size: 22,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+        body: ListView(
+          physics: const BouncingScrollPhysics(),
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          children: [
+            _item('TAG:', _vmException.tag),
+            const SizedBox(height: 15),
+            _item('Create time:', _vmException.time),
+            const SizedBox(height: 15),
+            _item('Call func name:', _vmException.callFuncName),
+            const SizedBox(height: 15),
+            _item('Line:', _vmException.line),
+            const SizedBox(height: 15),
+            _item('Base request', _vmException.baseRequest),
+            const SizedBox(height: 15),
+            _item('Response status code:', _vmException.responseStatusCode),
+            const SizedBox(height: 15),
+            _item('Response phrase:', _vmException.responsePhrase),
+            const SizedBox(height: 15),
+            _item('Response body:', _vmException.responseBody),
+            const SizedBox(height: 15),
+            _item('Token is valid:', _vmException.tokenIsValid),
+            const SizedBox(height: 15),
+            _item('Message:', _vmException.message)
+          ],
+        ),
       ),
     );
   }
